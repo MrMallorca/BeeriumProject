56096d70e024d628c0050ef6ce162ffa3a0ebc6d
EdgarEscutia <edgarescutiaperal@gmail.com>
2025-01-17 10:57:04 +0100

    IDamageable

--- Assets/enemyHealth.cs
+++ Assets/enemyHealth.cs
@@ -1,12 +1,28 @@
 using UnityEngine;
 
-public class enemyHealth : MonoBehaviour
+public class enemyHealth : MonoBehaviour, IDamageable
 {
     [SerializeField] private float maxHealth;
 
     private float currentHealth;
+
+
+
     private void Start()
     {
         currentHealth = maxHealth;
+    }
+    public void Damage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    { 
+        Destroy(gameObject);
     }
 }
