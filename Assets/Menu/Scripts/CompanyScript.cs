62eefcc621239c5f74d6811e1ac66fba0350e110
EdgarEscutia <edgarescutiaperal@gmail.com>
2025-02-12 13:27:46 +0100

    company scene done

--- Assets/Menu/Scripts/CompanyScript.cs
+++ Assets/Menu/Scripts/CompanyScript.cs
@@ -1,10 +1,24 @@
+using System;
+using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class CompanyScript : MonoBehaviour
 {
-    public void ChangeScene(string name)
+    [SerializeField] string name;
+    
+    void Start()
+    {
+        Wait();
+        ChangeScene();
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSecondsRealtime(100f);
+    }
+    public void ChangeScene()
     {
         SceneManager.LoadScene(name);
     }
