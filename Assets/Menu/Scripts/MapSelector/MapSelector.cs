6a05ff6eb710a9596196a285bc29683bcc7cab64
EdgarEscutia <edgarescutiaperal@gmail.com>
2025-02-14 10:56:47 +0100

    last viernes (14/02/25)

--- Assets/Menu/Scripts/MapSelector/MapSelector.cs
+++ Assets/Menu/Scripts/MapSelector/MapSelector.cs
@@ -5,50 +5,5 @@ using UnityEngine;
 
 public class MapSelector : MonoBehaviour
 {
-    public List<Kingdom> kingdoms;
-
-    private GameObject kingdomPointPrefab;
-    private Transform modelTransform;
-
-    private void Start()
-    {
-        foreach(Kingdom k in kingdoms)
-        {
-            SpawnKingdomPoint(k);
-        }
-    }
-
-    private void SpawnKingdomPoint(Kingdom k)
-    {
-        GameObject kingdom = Instantiate(kingdomPointPrefab, modelTransform);
-        kingdom.transform.localEulerAngles = new Vector3(k.x, k.y, 0);
-    }
-
-    public void LookAtKingdom(Kingdom k)
-    {
-        Transform cameraParent = Camera.main.transform.parent;
-        Transform cameraPivot = cameraParent.parent;
-
-        cameraParent.localEulerAngles = new Vector3(k.x, 0, 0);
-        cameraPivot.localEulerAngles = new Vector3(cameraPivot.localEulerAngles.x, k.y,0);
-    }
-
-    private void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.E))
-        {
-            LookAtKingdom(kingdoms[0]);
-        }
-        if(Input.GetKeyDown(KeyCode.R))
-        {
-            LookAtKingdom(kingdoms[1]);
-        }
-    }
-}
-
-[System.Serializable]
-public class Kingdom
-{
-    public string name;
-    public float x, y;
+    
 }
