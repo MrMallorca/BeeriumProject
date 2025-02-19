07200fa5f5287202ec62d772ce804f2651f9ca17
EdgarEscutia <edgarescutiaperal@gmail.com>
2025-02-19 14:02:44 +0100

    casi finish map selector

---
+++ Assets/MapSelectorCameraController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using Unity.Cinemachine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class MapSelectorCameraController : MonoBehaviour
+{
+    [SerializeField] CinemachineCamera Yellowcamera;
+    [SerializeField] CinemachineCamera Pinkcamera;
+    [SerializeField] CinemachineCamera Greencamera;
+    [SerializeField] CinemachineCamera Startcamera;
+
+    [SerializeField] Button YellowBtn;
+    [SerializeField] Button PinkBtn;
+    [SerializeField] Button GreenwBtn;
+
+
+    private void Start()
+    {
+        Yellowcamera.enabled = false;
+        Pinkcamera.enabled = false;
+        Greencamera.enabled = false;
+        Startcamera.enabled = true;
+    }
+    public void OnClickYellow()
+    {
+        Debug.Log("Yellow camera enable");
+
+        Yellowcamera.enabled = true;
+        Pinkcamera.enabled = false;
+        Greencamera.enabled = false;
+        Startcamera.enabled = false;
+    }
+
+    public void OnClickGreen()
+    {
+        Debug.Log("Green camera enable");
+
+        Yellowcamera.enabled = false;
+        Pinkcamera.enabled = false;
+        Startcamera.enabled = false;
+        Greencamera.enabled = true;
+    }
+    public void OnClickPink()
+    {
+        Debug.Log("Pink camera enable");
+
+        Yellowcamera.enabled = false;
+        Pinkcamera.enabled = true;
+        Greencamera.enabled = false;
+        Startcamera.enabled = false;
+    }
+}
