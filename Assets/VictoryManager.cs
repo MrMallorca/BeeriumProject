7c0fc628e82f0c5182f89f8bc4389a7ca35ecdaf
EdgarEscutia <edgarescutiaperal@gmail.com>
2025-02-21 10:30:31 +0100

    start EndGame

---
+++ Assets/VictoryManager.cs
@@ -0,0 +1,23 @@
+using TMPro;
+using UnityEngine;
+
+public class VictoryManager : MonoBehaviour
+{
+    [SerializeField] TMP_Text Player1Victory;
+    [SerializeField] TMP_Text Player2Victory;
+
+    public HealthBar instance;
+
+    private void Awake()
+    {
+
+    }
+    void Update()
+    {
+        if(HealthBar.instance.slider.value <= 0)
+        {
+            Debug.Log("PUTAAAAAAA");
+            Player1Victory.enabled = true;
+        }
+    }
+}
