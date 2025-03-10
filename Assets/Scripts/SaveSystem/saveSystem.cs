409fd9b676123ca767bf2ac6e016b453fe25bbc8
MrMallorca <37783709+MrMallorca@users.noreply.github.com>
2025-03-10 12:20:14 +0100

    a

--- Assets/Scripts/SaveSystem/saveSystem.cs
+++ Assets/Scripts/SaveSystem/saveSystem.cs
@@ -1,14 +1,69 @@
 using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEditor;
 
 public class saveSystem : MonoBehaviour
 {
-    PlayerMovement2 playerMove;
+    Diana1 dianaSave;
     HealthBar healthBar;
+
+    string savePath;
+    saveData data;
 
     private void Awake()
     {
-        healthBar = FindObjectOfType<HealthBar>();
+        dianaSave = FindAnyObjectByType<Diana1>();
+        healthBar = FindAnyObjectByType<HealthBar>();
+
+        savePath = Application.persistentDataPath + "/save.dat";
+        if (!File.Exists(savePath))
+        {
+            saveData newData = new saveData();
+            newData.playerPosition = Vector2.zero;
+            newData.playerHealth = 100;
+
+            SaveGame(newData);
+        }
+        data = LoadGame();
     }
+    void SaveGame(saveData dataToSave)
+    {
+        string JsonData = JsonUtility.ToJson(dataToSave);
+        File.WriteAllText(savePath, JsonData);
+    }
+
+    saveData LoadGame()
+    {
+        string loaderData = File.ReadAllText(savePath);
+        saveData dataToReturn = JsonUtility.FromJson<saveData>(loaderData);
+
+        return dataToReturn;
+    }
+
+    public void SaveGameButton()
+    {
+        Vector2 playerPos = dianaSave.GetPosition();
+        float health = healthBar.GetHealth();
+
+        data.playerPosition = playerPos;
+        data.playerHealth = health;
+
+        SaveGame(data);
+    }
+
+    public void LoadGameButton()
+    {
+        Vector2 playerPos = data.playerPosition;
+        float health = data.playerHealth;
+
+        dianaSave.SetPosition(playerPos);
+        healthBar.SetHealth(health);
+    }
+}
+
+public class saveData
+{
+    public Vector2 playerPosition;
+    public float playerHealth;
 }
