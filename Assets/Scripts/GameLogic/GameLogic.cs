463c4da969fec2308b58e4b26267337bc5f5e29e
MrMallorca <37783709+MrMallorca@users.noreply.github.com>
2025-03-24 12:56:14 +0100

    fix game logic

--- Assets/Scripts/GameLogic/GameLogic.cs
+++ Assets/Scripts/GameLogic/GameLogic.cs
@@ -22,33 +22,25 @@ public class GameLogic : MonoBehaviour
         {
             string personaje1 = CharacterSelectorManager.confirmedCharacter1;
             GameObject personaje = personajes.Find((x) => x.name == personaje1);
-            InitCharacter1(personaje, player1.transform, actionSetPl1, "Player1");
+            InitCharacter(personaje, player1.transform, actionSetPl1, "Player1");
         }
 
         {
             string personaje2 = CharacterSelectorManager.confirmedCharacter2;
             GameObject personaje = personajes.Find((x) => x.name == personaje2);
-            InitCharacter2(personaje, player2.transform, actionSetPl2, "Player2");
+            InitCharacter(personaje, player2.transform, actionSetPl2, "Player2");
         }
     }
 
-    private void InitCharacter1(GameObject personaje, Transform playerTransform, PlayerMovements.ActionSet actionSet, string tag)
+    private void InitCharacter(GameObject personaje, Transform playerTransform, PlayerMovements.ActionSet actionSet, string tag)
     {
         GameObject player = Instantiate(personaje, playerTransform.position, Quaternion.identity, playerTransform);
         player.tag = tag;
 
         BaseFighter baseFighter = player.GetComponent<BaseFighter>();
-        baseFighter.InitInputs(actionSetPl1);
+        baseFighter.InitInputs(actionSet);
 
     }
 
-    private void InitCharacter2(GameObject personaje, Transform playerTransform, PlayerMovements.ActionSet actionSet, string tag)
-    {
-        GameObject player = Instantiate(personaje, playerTransform.position, Quaternion.identity, playerTransform);
-        player.tag = tag;
-
-        BaseFighter baseFighter = player.GetComponent<BaseFighter>();
-        baseFighter.InitInputs(actionSetPl2);
-
-    }
+  
 }
