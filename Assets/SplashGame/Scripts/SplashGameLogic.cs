a9846df440826935ca80804beef2f042bee89a95
MrMallorca <eduardojosedelgadoguzman@gmail.com>
2025-04-15 17:59:02 +0200

    splash game avanzado

--- Assets/SplashGame/Scripts/SplashGameLogic.cs
+++ Assets/SplashGame/Scripts/SplashGameLogic.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using EasyTransition;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -9,24 +10,62 @@ public class SplashGameLogic : MonoBehaviour
     [SerializeField] TransitionSettings transitionSettings;
     public float startDelay;
 
-    private bool isTransitioning = false;
+    private bool transitionIsFinish = false;
+
+    [SerializeField] CanvasGroup[] images;
+    [SerializeField] TextMeshProUGUI title;
 
 
-    
-
-    
-    private void Update()
+    private void Start()
     {
-        if (isTransitioning)
-        {
-            return;
-        }
-
-        isTransitioning = true;
-        TransitionManager.Instance().Transition(nextSceneName, transitionSettings, startDelay);
+        StartCoroutine(FadeImagesSequentially());
 
 
     }
 
+    private void Update()
+    {
+        if(Input.anyKeyDown && transitionIsFinish)
+        {
+            TransitionManager.Instance().Transition(nextSceneName, transitionSettings, startDelay);
+        }
+    }
 
+    private IEnumerator BlinkText()
+    {
+        while (true)
+        {
+            title.text = " ";
+            yield return new WaitForSeconds(0.5f);
+            title.text = "The Last Stars";
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private IEnumerator FadeImagesSequentially()
+    {
+        yield return new WaitForSeconds(startDelay);
+
+        foreach (CanvasGroup image in images)
+        {
+            float t = 0f;
+            image.alpha = 0f;
+
+            while (t < 1f)
+            {
+                t += Time.deltaTime;
+                image.alpha = Mathf.Clamp01(t); 
+                yield return null;
+            }
+
+            image.alpha = 1f; 
+
+            yield return new WaitForSeconds(0.3f);
+        }
+        transitionIsFinish = true;
+
+        StartCoroutine(BlinkText());
+
+
+    }
 }
