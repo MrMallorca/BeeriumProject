1434d34453e0ae06335f8e4f20274aa01a3d8c3d
ElÑeño <nilortegaromera@gmail.com>
2025-05-07 13:09:14 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Assets/MapSelector/Scripts/changeScene.cs
+++ Assets/MapSelector/Scripts/changeScene.cs
@@ -1,8 +1,16 @@
+using EasyTransition;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class changeScene : MonoBehaviour
 {
+    [Header("Cambio Escena")]
+    public TransitionSettings transition;
+    public float loadDelay;
     public void ChangeScene(string name)
-    { SceneManager.LoadScene(name); }
+    {
+        //SceneManager.LoadScene(name);
+        TransitionManager.Instance().Transition(name, transition, loadDelay);
+
+    }
 }
